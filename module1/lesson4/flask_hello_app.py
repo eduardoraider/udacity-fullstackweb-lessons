4648390b4db8ca0ff0e1474fabee176b931c6e3b
Eduardo Raider <eduraider@gmail.com>
2022-02-27 19:43:16 -0300

    SQLAlchemy Basics

---
+++ module1/lesson4/flask_hello_app.py
@@ -0,0 +1,24 @@
+import imp
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost:5432/example'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Person(db.Model):
+    __tablename__ = 'persons'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+
+    def __repr__ (self):
+        return f'<Person ID: {self.id}, name: {self.name}>'
+
+db.create_all()    
+
+@app.route('/')
+def index():
+    person = Person.query.first()
+    return 'Hello ' + person.name
