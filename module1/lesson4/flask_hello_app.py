eaa9e6c067846f8467b4e539ad72570bfbd0cd34
Eduardo Raider <eduraider@gmail.com>
2022-03-01 00:06:37 -0300

    Todo App

--- module1/lesson4/flask_hello_app.py
+++ module1/lesson4/flask_hello_app.py
@@ -1,4 +1,4 @@
-import imp
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
---
+++ module1/todoapp/app.py
@@ -0,0 +1,25 @@
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost:5432/todoapp'
+
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__ (self):
+        return f'<Todo ID: {self.id}, description: {self.description}>'
+
+#db.create_all()    
+
+@app.route('/')
+def index():
+    return 'Hello World' 
