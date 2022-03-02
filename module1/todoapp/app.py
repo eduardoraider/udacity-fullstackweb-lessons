6f9ea1f621f0e826b28d0b69c3b809a80242796c
Eduardo Raider <eduraider@gmail.com>
2022-03-01 21:49:51 -0300

    Todo App

--- module1/todoapp/app.py
+++ module1/todoapp/app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -21,6 +21,27 @@ class Todo(db.Model):
 
 #db.create_all()    
 
+@app.route('/todos/create', methods=['POST'])
+def create_todo():   
+   body={}
+   error = False
+   try: 
+       description =  request.get_json()['description']
+       todo = Todo(description=description)
+       body['description'] = todo.description
+       db.session.add(todo)
+       db.session.commit()
+   except:        
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+   finally:
+        db.session.close()           
+        if  error == True:
+            abort(400)
+        else:            
+            return jsonify(body)
+
 @app.route('/')
 def index():
-    return 'Hello World' 
+    return render_template('index.html', data=Todo.query.all()) 
