0ed8cb4febe92f163d227b38ea67ffd19abc5cf1
Eduardo Raider <eduraider@gmail.com>
2022-03-03 01:09:21 -0300

    Todo App

--- todoapp/app.py
+++ todoapp/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost:5432/todoapp'
@@ -15,6 +16,29 @@ class Todo(db.Model):
         return f'<Todo {self.id} {self.description}>'
 
 db.create_all()
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    error = False
+    body = {}
+    try:
+        # description = request.form.get('description', '')
+        description = request.get_json()['description']
+        todo = Todo(description=description)
+        db.session.add(todo)
+        db.session.commit()
+        body['description'] = todo.description
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info)
+    finally:
+        db.session.close()
+    if error:
+        abort(400)
+    else:
+        #return redirect(url_for('index))
+        return jsonify(body)   
 
 @app.route('/')
 def index():
