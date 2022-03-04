067094d7d49361d48786cb6b8dae1fccde567403
Eduardo Raider <eduraider@gmail.com>
2022-03-04 01:29:50 -0300

    Todo App

--- todoapp/app.py
+++ todoapp/app.py
@@ -11,11 +11,24 @@ class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
-
+    completed = db.Column(db.Boolean, nullable=False, default=False)
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
 db.create_all()
+
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
@@ -42,6 +55,6 @@ def create_todo():
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.all())
+    return render_template('index.html', data=Todo.query.order_by('id').all())
 
 
